7d1f0b622cf07ba2ee8034ab582060308b6f8bea
Nenad Stokic <nstokic@gmail.com>
2019-12-15 20:31:45 +0100

    added sequelize branch, other lectures with their branches will come after

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 
-server.listen(3000);
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+sequelize
+.sync()
+.then(result => {
+  app.listen(3000);
+})
+.catch(err => {
+  console.log(err);
+});
