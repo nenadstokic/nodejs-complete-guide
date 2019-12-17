877d7186b3d2d6bdd4adc945ac9f0e91b390a0f5
Nenad <nstokic@gmail.com>
2019-12-17 23:38:42 +0100

    added cart and cart-item models - I think Maximillian is wrong at this point

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,9 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  
+  req.user
+  .createProduct({
     title:title,
     price: price,
     imageUrl: imageUrl,
@@ -53,8 +55,12 @@ exports.getEditProduct = (req, res, next) => {
     console.log(err);
   });
   */
-  Product.findByPk(prodId)
-  .then(product => {
+
+  //Product.findByPk(prodId)
+  req.user
+  .getProducts({ where: { id: prodId } })
+  .then(products => {
+    const product = products[0];
     if (!product) {
       return res.redirect('/');
     }
@@ -97,7 +103,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  //Product.findAll()
+  req.user
+  .getProducts()
   .then(products => {
     res.render('admin/products', {
       prods: products,
