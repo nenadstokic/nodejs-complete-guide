11151240abe7ed9c1b9a242326617236c0622da9
Nenad <nstokic@gmail.com>
2020-01-06 13:51:47 +0100

    adding products

--- util/database.js
+++ util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'DicaBoga!2020', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://nenad:DicaBoga_2020_mongo@cluster0-dlyqr.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Connected');
+    _db = client.db();
+    callback();
+  })
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
+}
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
