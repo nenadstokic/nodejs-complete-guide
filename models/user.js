1757a414180740540026ff6b9c0100f46fd5c5a8
Nenad <nstokic@gmail.com>
2020-01-20 01:00:14 +0100

    Finished mongodb module

--- models/user.js
+++ models/user.js
@@ -92,17 +92,34 @@ class User {
 
   addOrder() {
     const db = getDb();
+    return this.getCart()
+    .then(products => {
+      const order = {
+        items: products,
+        user: {
+          _id: new ObjectId(this._id),
+          name: this.name
+        }
+      };
+      return db.collection('orders').insertOne(order);
+    })
+    .then(result => {
+      this.cart = { items: [] };
+      return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id)}, 
+        { $set: { cart: {items: [] } } }
+      );
+    });
+  }
+
+  getOrders() {
+    const db = getDb();
     return db
-      .collection('orders')
-      .insertOne(this.cart)
-      .then(result => {
-        this.card = { items: [] };
-        return db
-        .collection('users')
-        .updateOne(
-          { id: new ObjectId(this._id)}, 
-          { $set: { cart: {items: [] } } })
-      });
+    .collection('orders')
+    .find({'user._id': new ObjectId(this._id)})
+    .toArray();
   }
 
   static findById(userId) {
