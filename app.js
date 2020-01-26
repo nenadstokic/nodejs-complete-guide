529dd280dff15e2e5d786ed2f6738b6074e4c5ab
Nenad Stokic <nstokic@gmail.com>
2020-01-26 19:06:31 +0100

    finished fetching all products

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+//const User = require('./models/user');
+
 
 const app = express();
 
@@ -18,22 +20,23 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5e1399437e93d9334897f0b7')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      console.log(req.user);
+// app.use((req, res, next) => {
+//   User.findById('5e1399437e93d9334897f0b7')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       console.log(req.user);
       
-      next();
-    })
-    .catch(err => console.log(err));
-});
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://nenad:DicaBoga_2020_mongo@cluster0-dlyqr.mongodb.net/shop?retryWrites=true&w=majority').then(result => {
   app.listen(3000);
-});
+})
+.catch(err => console.log(err));
